7e56a9c7958d7e392cd36ae20124d6661e4c9226
Ines Goethals <ines.goethals@student.ehb.be>
2024-10-02 22:21:17 +0200

    changed to linewith

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,44 @@
-"use strict";
+let canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext("2d");
 
-alert("Draw your space invader here");
+drawLine();
+
+function drawLine() {
+  context.fillStyle = "#8B94CA";
+  context.beginPath();
+  context.rect(400, 100, 200, 100);
+  context.fill();
+  context.stroke();
+
+  context.fillStyle = "#8B94CA";
+  context.beginPath();
+  context.rect(600, 200, 100, 100);
+  context.fill();
+  context.stroke();
+
+  context.fillStyle = "#8B94CA";
+  context.beginPath();
+  context.rect(700, 100, 200, 100);
+  context.fill();
+  context.stroke();
+
+  context.fillStyle = "#8B94CA";
+  context.beginPath();
+  context.rect(400, 300, 500, 100);
+  context.fill();
+  context.stroke();
+
+  context.fillStyle = "#8B94CA";
+  context.beginPath();
+  context.rect(400, 500, 200, 100);
+  context.fill();
+  context.stroke();
+
+  context.fillStyle = "#8B94CA";
+  context.beginPath();
+  context.rect(700, 500, 200, 100);
+  context.fill();
+  context.stroke();
+}
